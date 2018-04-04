0a29d5fa2868d59e0209834516f0165ee63ab30f
Sanjay Kumar Mahto <lpmb-bang-sanjay@Sanjays-MacBook-Pro.local>
2018-04-04 16:40:45 +0530

    Added eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
 module.exports = {
     "rules": {
-        "no-param-reassign": 0
+        "no-param-reassign": 0,
+        "import/no-mutable-exports":0,
+        "no-return-assign": 0, 
+        "no-multi-assign": 0,
+        "no-underscore-dangle": 0
     },
     "env": {
         "browser": true,
