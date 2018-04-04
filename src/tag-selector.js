b16b693bc25545c7ce0a30090c85d8028f17cddf
Sanjay Kumar Mahto <lpmb-bang-sanjay@Sanjays-MacBook-Pro.local>
2018-04-04 16:44:36 +0530

    Added tag-selector file

---
+++ src/tag-selector.js
@@ -0,0 +1,23 @@
+import loadData from './data-loader';
+/**
+ * @description Function to append SVG element to Selected Div element.
+ * @param  {} mount='body'
+ */
+let divElement;
+let margin;
+let width;
+let height;
+const dag = (mount = 'body') => {
+  divElement = mount;
+  // Set the dimensions and margins of the diagram
+  margin = {
+    top: 20, right: 90, bottom: 30, left: 90,
+  };
+  width = 960 - margin.left - margin.right;
+  height = 500 - margin.top - margin.bottom;
+
+  return loadData;
+};
+
+window.dag = dag;
+export { divElement, margin, width, height, dag };
