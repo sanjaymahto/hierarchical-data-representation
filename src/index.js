bc1c1a3a49edd8f21b12a80353f524ffdbe96120
Sanjay Kumar Mahto <lpmb-bang-sanjay@Sanjays-MacBook-Pro.local>
2018-04-04 16:44:00 +0530

    copied code from index file to tag-selector file

--- src/index.js
+++ src/index.js
@@ -1,25 +1,3 @@
-import loadData from './data-loader';
-/**
- * @description Function to append SVG element to Selected Div element.
- * @param  {} mount='body'
- */
-/* eslint import/no-mutable-exports:0 */
-let divElement;
-let margin;
-let width;
-let height;
-const dag = (mount = 'body') => {
-  divElement = mount;
-  // Set the dimensions and margins of the diagram
-  margin = {
-    top: 20, right: 90, bottom: 30, left: 90,
-  };
-  width = 960 - margin.left - margin.right;
-  height = 500 - margin.top - margin.bottom;
+import dag from './tag-selector';
 
-  return loadData;
-};
-
-window.dag = dag;
-
-export { divElement, margin, width, height };
+export default dag;
