3a9963059c251541e49e4761407ad0a9ed2cf980
Sanjay Kumar Mahto <lpmb-bang-sanjay@Sanjays-MacBook-Pro.local>
2018-04-09 16:11:53 +0530

    file name renamed from tag-selector to dag.js

---
+++ src/dag.js
@@ -0,0 +1,27 @@
+import loadData from './data-loader';
+/**
+ * @description Function to append SVG element to Selected Div element.
+ * @param  {} mount='body'
+ */
+let divElement;
+let margin;
+let width;
+let height;
+let nodeSize;
+const dag = (mount = 'body', elemMargin = {
+  top: 50, right: 90, bottom: 30, left: 500,
+}, elemWidth = 960, elemHeight = 500, elemNodeSize = 30) => {
+  divElement = mount;
+
+  // Set the dimensions and margins of the diagram
+  margin = {
+    top: elemMargin.top, right: elemMargin.right, bottom: elemMargin.bottom, left: elemMargin.left,
+  };
+  width = elemWidth - margin.left - margin.right;
+  height = elemHeight - margin.top - margin.bottom;
+  nodeSize = elemNodeSize;
+  return loadData;
+};
+
+export { dag as default };
+export { divElement, margin, width, height, nodeSize };
