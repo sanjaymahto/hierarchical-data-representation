bd84c33f1e51ad1c7f9115c8a3d2fff7ed4c49c4
Sanjay Kumar Mahto <lpmb-bang-sanjay@Sanjays-MacBook-Pro.local>
2018-04-09 16:13:25 +0530

    Webpack file modified

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,25 @@
-const path = require('path');
+const libraryName = 'dag';
+const OUT_FILE = `${libraryName}.js`;
 
 module.exports = {
   entry: {
     bundle: './src/index.js',
   },
   output: {
-    path: path.resolve(__dirname, 'build'),
-    publicPath: '/dist/',
-    filename: 'bundle.js',
-  },
-  devServer: {
-    inline: true,
-    contentBase: './example',
+    path: `${__dirname}/dist`,
+    filename: OUT_FILE,
+    library: libraryName,
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
   },
   module: {
     rules: [
       { test: /\.js$/, use: 'babel-loader' },
     ],
+  },
+  devServer: {
+    inline: true,
+    contentBase: './example',
   },
 };
 
