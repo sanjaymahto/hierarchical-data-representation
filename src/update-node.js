fb91fc3baadf44223529c4e5321ae2ddbf366374
Sanjay Kumar Mahto <lpmb-bang-sanjay@Sanjays-MacBook-Pro.local>
2018-04-13 13:31:47 +0530

    Refactored the code

--- src/update-node.js
+++ src/update-node.js
@@ -34,7 +34,6 @@ export default function updateNode(rendersvg, root, rootElement, renderTreemap, 
 
   // Normalize for fixed-depth.
   nodes.forEach((d) => { d.y = d.depth * 90; });
-
   // ****************** Nodes section *************************** //
   // Update the nodes...
   const node = rendersvg.selectAll('g.node')
