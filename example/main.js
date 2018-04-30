d9314d376e2573895ec34a361b370593e51000dc
Sanjay Kumar Mahto <lpmb-bang-sanjay@Sanjays-MacBook-Pro.local>
2018-04-30 22:34:27 +0530

    refactored the code

--- example/main.js
+++ example/main.js
@@ -2,7 +2,7 @@
 
 const create = dag.default;
 
-let dagFn = create('#dag');
+let dagFn = create('#dag'); // Mounting DAG mount element.
 
 let instance = dagFn({
   "host": null,
@@ -94,7 +94,173 @@ let instance = dagFn({
     }
   ]
 }, {
+  margin: {
+    top: 50,
+    right: 90,
+    bottom: 30,
+    left: 700,
+  },
+  width: 960,
+  height: 400,
+  nodeSize: 30,
     children: 'lanes',
     foldable: true
+  });                       //Creating an instance.
+
+instance.render();          // calling render method to render the DAG graph.
+
+//Function to reset the DAG graph
+function reset(){
+  location.reload();
+}
+
+//Function to add names to the node.
+function addName() {
+  instance.nodeName((d) =>{
+    return d.data.nodeid;
   });
-instance.render();
+};
+
+//function to add path names to the node.
+function addpathName() {
+  instance.pathName('instance', 'pathText');
+};
+
+//function to Update the Data.
+function updateData(){
+  instance.updateData({
+    "host": null,
+    "cut": "v",
+    "ratioWeight": 1,
+    "nodeid": 1,
+    "pathText": "This is root Node.",
+    "lanes": [
+      {
+        "host": null,
+        "cut": "h",
+        "ratioWeight": 1,
+        "nodeid": 2,
+        "preferred": true,
+        "pathText": "This is first Node.",
+        "lanes": [
+          {
+            "host": {
+              "seed": 10,
+              "dimensions": {
+                "width": 300,
+                "height": 300
+              },
+              "position": null
+            },
+            "cut": null,
+            "ratioWeight": 1,
+            "pathText": "This is third Node.",
+            "nodeid": 4,
+            "preferred": true,
+            "lanes": []
+          },
+          {
+            "host": {
+              "seed": 10,
+              "dimensions": {
+                "width": 300,
+                "height": 300
+              },
+              "position": null
+            },
+            "cut": null,
+            "ratioWeight": 1,
+            "pathText": "This is fourth Node.",
+            "nodeid": 5,
+            "lanes": []
+          },
+      {
+            "host": {
+              "seed": 10,
+              "dimensions": {
+                "width": 300,
+                "height": 300
+              },
+              "position": null
+            },
+            "cut": null,
+            "ratioWeight": 1,
+            "pathText": "This is fifth Node.",
+            "nodeid": 6,
+            "lanes": []
+          }
+        ]
+      },
+      {
+        "host": null,
+        "cut": "h",
+        "ratioWeight": 1,
+        "nodeid": 3,
+        "pathText": "This is second Node.",
+        "lanes": [
+          {
+            "host": {
+              "seed": 10,
+              "dimensions": {
+                "width": 300,
+                "height": 300
+              },
+              "position": null
+            },
+            "cut": null,
+            "ratioWeight": 1,
+            "pathText": "This is sixth Node.",
+            "nodeid": 7,
+            "preferred": true,
+            "lanes": []
+          }
+        ]
+      }
+    ]
+  });
+} 
+
+//Function to add event to nodes.
+function addEvent(){
+  instance.on('click', (d) => {
+    alert(d.data.nodeid);
+  })
+
+  instance.on('mouseover', (d) => {
+    console.log(d.data);
+  })
+};
+
+//Function to remove all events to nodes.
+function removeAllEvents(){
+  instance.removeEvent();
+};
+
+//Function to remove event to nodes.
+function removeEvents(){
+  instance.removeEvent(['click', 'mouseover']);
+};
+
+//Function to collapse DAG tree according to levels.
+function collapseLevel(){
+  instance.expand(0);
+  instance.collapse(1);
+};
+
+//Function to collapse DAG tree according to levels and sibling nodes.
+function collapseLevelwithSibling(){
+  instance.expand(0);
+  instance.collapse(1, [0]);
+}
+
+//Function to expand DAG tree according to levels.
+function expandLevel(){
+  instance.collapse(1);
+  instance.expand(1);
+};
+
+//Function to expand DAG tree according to levels and sibling nodes.
+function ExpandLevelwithSibling(){
+  instance.collapse(1);
+  instance.expand(1, [0]);
+}
